4ddf81aac3966aa30b644ec25df4cf0e505cb185
Andy McKenzie <amckenz@gmail.com>
2015-03-21 22:05:22 -0400

    Create defib.py
    i think this is literally the most basic start possible to this game

---
+++ defib.py
@@ -0,0 +1,25 @@
+import sys, math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+LON = raw_input()
+print(type(LON))
+LON_dot = LON.replace(",", ".") 
+print LON_dot
+LAT = raw_input()
+N = int(raw_input())
+for i in xrange(N):
+    DEFIB = raw_input()
+    
+for i in xrange(0, len(DEFIB)): 
+    if i == 0: 
+        #print DEFIB[2]
+        pass
+    
+#print DEFIB
+
+# Write an action using print
+# To debug: print >> sys.stderr, "Debug messages..."
+
+#print "answer"
